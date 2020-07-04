21710056aad7ab0682cafd094af761b32c91771b
LeadKiller <hl2epicfan@gmail.com>
2020-07-04 13:32:20 -0700

    kamikaze and flemming get stuck less

--- lua/entities/npc_flemming/init.lua
+++ lua/entities/npc_flemming/init.lua
@@ -4,154 +4,159 @@ AddCSLuaFile( "shared.lua" )
 include( "shared.lua" )
 
 function ENT:Initialize()
-	self:SetModel( "models/humans/group01/male_04.mdl" )
-	util.PrecacheSound("npc/zombie/zo_attack2.wav")
-	self.IsBob = true
-	self.Width = self:BoundingRadius() * 1
+    self:SetModel( "models/humans/group01/male_04.mdl" )
+    util.PrecacheSound("npc/zombie/zo_attack2.wav")
+    self.IsBob = true
+    self.Width = self:BoundingRadius() * 1
 
-	self.Speed = 200
-	self.Swinged = false
-	self.SwingedVoice = false
-	self.SwingTimer = 0
-	self.VoiceTimer = 0
+    self.Speed = 200
+    self.Swinged = false
+    self.SwingedVoice = false
+    self.SwingTimer = 0
+    self.VoiceTimer = 0
 
-	self:SetHealth(math.random(92220,112220))
+    self:SetHealth(math.random(92220,112220))
 
-	-- do this serverside so hitbox can match up
-	self:ManipulateBoneScale(self:LookupBone("ValveBiped.Bip01_Head1"), Vector(2,2,2))
+    -- do this serverside so hitbox can match up
+    self:ManipulateBoneScale(self:LookupBone("ValveBiped.Bip01_Head1"), Vector(2,2,2))
 end
 
 function ENT:Think()
-	self:FindEnemy()
-	--self:DidHit()
-	self:Hit()
-	if self.SwingTimer ~= 0 and self.SwingTimer < CurTime() then
-		self.SwingTimer = 0
-		--print("Finished.")
-		self.Swinged = false
-		self.SwingedVoice = false
-		self:StartActivity(ACT_RUN)
-		self.Speed = 200
-		self.loco:SetDesiredSpeed(200)
-		return
-	end
-	--self:StartActivity( ACT_RUN )
-	if self.Swinged == true and self.SwingTimer == 0 then
-		self.loco:FaceTowards(self:GetNWEntity("enemy"):GetPos())
-		--self:SetAngles(Angle(self:GetAngles().p, self:GetNWEntity("enemy"):GetPos():Angle().yaw, self:GetAngles().r))
-		self.Speed = 0
-		self.loco:SetDesiredSpeed(0)
-		self:StartActivity(ACT_IDLE)
-		self:ResetSequenceInfo()
-		self:ResetSequence("swing")
-		self:SetPlaybackRate(3)
-		self:EmitSound("vo/npc/male01/hi0"..math.random(1,2)..".wav")
-		self.SwingTimer = CurTime() + self:SequenceDuration()
-	end
+    self:FindEnemy()
+    --self:DidHit()
+    self:Hit()
+    if self.SwingTimer ~= 0 and self.SwingTimer < CurTime() then
+        self.SwingTimer = 0
+        --print("Finished.")
+        self.Swinged = false
+        self.SwingedVoice = false
+        self:StartActivity(ACT_RUN)
+        self.Speed = 200
+        self.loco:SetDesiredSpeed(200)
+        return
+    end
+    --self:StartActivity( ACT_RUN )
+    if self.Swinged == true and self.SwingTimer == 0 then
+        self.loco:FaceTowards(self:GetNWEntity("enemy"):GetPos())
+        --self:SetAngles(Angle(self:GetAngles().p, self:GetNWEntity("enemy"):GetPos():Angle().yaw, self:GetAngles().r))
+        self.Speed = 0
+        self.loco:SetDesiredSpeed(0)
+        self:StartActivity(ACT_IDLE)
+        self:ResetSequenceInfo()
+        self:ResetSequence("swing")
+        self:SetPlaybackRate(3)
+        self:EmitSound("vo/npc/male01/hi0"..math.random(1,2)..".wav")
+        self.SwingTimer = CurTime() + self:SequenceDuration()
+    end
 end
 
 function ENT:RunBehaviour()
-	while true do
-		if self.Swinged == false then
-			self:StartActivity( ACT_RUN )
-			self:Move()
-			self:EmitSound("vo/npc/male01/hi0"..math.random(1,2)..".wav")
-		end
-		coroutine.wait(2)
-	end
-end	
+    while true do
+        if self.Swinged == false then
+            self:StartActivity( ACT_RUN )
+            self:Move()
+            self:EmitSound("vo/npc/male01/hi0"..math.random(1,2)..".wav")
+        end
+        coroutine.wait(2)
+    end
+end    
 
 function ENT:Hit()
-	local players = player.GetAll()
-	for k, v in pairs(players) do
-		local distance = v:GetPos():Distance(self:GetPos())
-		if distance < 40 and v:Alive() and self.Swinged == false then
-			self.Swinged = true
-			v:EmitSound("Flesh.ImpactHard")
-			v:TakeDamage(1, self, self)
-		elseif distance > 40 then
-			self.SwingedVoice = false
-			if math.random(1,3) == 2 and self:Eyes() and self.VoiceTimer ~= 0 and self.VoiceTimer < CurTime() then
-				self:EmitSound("vo/npc/male01/hi0"..math.random(1,2)..".wav")
-				self.VoiceTimer = 0
-			elseif self.VoiceTimer == 0 then
-				self.VoiceTimer = CurTime() + 1
-			end
-		end
-	end
+    local players = player.GetAll()
+    for k, v in pairs(players) do
+        local distance = v:GetPos():Distance(self:GetPos())
+        if distance < 40 and v:Alive() and self.Swinged == false then
+            self.Swinged = true
+            v:EmitSound("Flesh.ImpactHard")
+            v:TakeDamage(1, self, self)
+        elseif distance > 40 then
+            self.SwingedVoice = false
+            if math.random(1,3) == 2 and self:Eyes() and self.VoiceTimer ~= 0 and self.VoiceTimer < CurTime() then
+                self:EmitSound("vo/npc/male01/hi0"..math.random(1,2)..".wav")
+                self.VoiceTimer = 0
+            elseif self.VoiceTimer == 0 then
+                self.VoiceTimer = CurTime() + 1
+            end
+        end
+    end
 end
 
 function ENT:Move()
-	if !IsValid(self:GetNWEntity("enemy")) then return end
-	local enemy = self:GetNWEntity("enemy")
-	self.loco:SetDesiredSpeed(self.Speed or 200)
-	local path = Path("Follow")
-	path:Compute(self, enemy:GetPos())
-	if !path:IsValid() then return end
-	while path:IsValid() and IsValid(enemy) do
-		if (path:GetAge() > 0.1) then
-			path:Compute(self, enemy:GetPos())
-		end
-		path:Update( self )
-		--path:Draw()
-		if (self.loco:IsStuck()) then
-			self:HandleStuck()
-			return
-		end
-		coroutine.yield()
-	end
-	return
+    if !IsValid(self:GetNWEntity("enemy")) then return end
+    local enemy = self:GetNWEntity("enemy")
+    self.loco:SetDesiredSpeed(self.Speed or 200)
+    local path = Path("Follow")
+    path:Compute(self, enemy:GetPos())
+    if !path:IsValid() then return end
+    while path:IsValid() and IsValid(enemy) do
+        if (path:GetAge() > 0.1) then
+            path:Compute(self, enemy:GetPos())
+        end
+        path:Update( self )
+
+        if path:GetAllSegments() and path:GetAllSegments()[1] and path:GetAllSegments()[2] and path:GetAllSegments()[2].pos.z > self:GetPos().z and path:GetAllSegments()[2].pos.z < self:GetPos().z + 24 and !(util.TraceHull({startpos = self:GetPos(), endpos = self:GetPos() + self:GetForward() * 50 + self:GetUp() * 24, mins = Vector(-15, -15, 0), maxs = Vector(15, 15, 72)}).Entity:IsPlayer() or self:GetPos():DistToSqr(enemy:GetPos()) < 2400) then
+            self:SetPos(self:GetPos() + path:GetAllSegments()[1].forward)
+        end
+
+        --path:Draw()
+        if (self.loco:IsStuck()) then
+            self:HandleStuck()
+            return
+        end
+        coroutine.yield()
+    end
+    return
 end
 
 function ENT:Eyes()
-	if !IsValid(self:GetNWEntity("enemy")) then return end
-	local enemy = self:GetNWEntity("enemy")
-	if !enemy:Alive() then return true end
-	local ViewEnt = enemy:GetViewEntity()
-	--if ViewEnt == self.Victim and not self:HasVictimLOS() then return false end
-	local fov = enemy:GetFOV()
-	local Disp = self:GetPos() - ViewEnt:GetPos()
-	local Dist = Disp:Length()
-	local MaxCos = math.abs( math.cos( math.acos( Dist / math.sqrt( Dist * Dist + self.Width * self.Width ) ) + fov * ( math.pi / 180 ) ) )
-	Disp:Normalize()
-	if Disp:Dot( ViewEnt:EyeAngles():Forward() ) > MaxCos then
-		return true
-	end
-	return false
+    if !IsValid(self:GetNWEntity("enemy")) then return end
+    local enemy = self:GetNWEntity("enemy")
+    if !enemy:Alive() then return true end
+    local ViewEnt = enemy:GetViewEntity()
+    --if ViewEnt == self.Victim and not self:HasVictimLOS() then return false end
+    local fov = enemy:GetFOV()
+    local Disp = self:GetPos() - ViewEnt:GetPos()
+    local Dist = Disp:Length()
+    local MaxCos = math.abs( math.cos( math.acos( Dist / math.sqrt( Dist * Dist + self.Width * self.Width ) ) + fov * ( math.pi / 180 ) ) )
+    Disp:Normalize()
+    if Disp:Dot( ViewEnt:EyeAngles():Forward() ) > MaxCos then
+        return true
+    end
+    return false
 end
 
 function ENT:FindEnemy()
-	local players = player.GetAll()
-	local distance = 9999
-	local player = player.GetAll()[1]
-	for k, v in pairs(players) do
-		local distanceplayer = v:GetPos():Distance(self:GetPos())
-		if distance > distanceplayer then
-			distance = distanceplayer
-			player = v
-		end
-	end
-	--print(player:Nick().." is the closest!")
-	self:SetNWEntity("enemy", player)
-	return player
+    local players = player.GetAll()
+    local distance = 9999
+    local player = player.GetAll()[1]
+    for k, v in pairs(players) do
+        local distanceplayer = v:GetPos():Distance(self:GetPos())
+        if distance > distanceplayer then
+            distance = distanceplayer
+            player = v
+        end
+    end
+    --print(player:Nick().." is the closest!")
+    self:SetNWEntity("enemy", player)
+    return player
 end
 
 function ENT:OnRemove()
-	local flemming = ents.Create(self:GetClass())
-	if !IsValid(self) then return end -- doesn't exist!
-	if !IsValid(flemming) then return end
-	flemming:SetPos(self:GetPos())
-	flemming:SetAngles(self:GetAngles())
-	flemming:SetMaterial(self:GetMaterial())
-	flemming:SetColor(self:GetColor())
-	flemming:SetName(self:GetName())
-	flemming:SetModel(self:GetModel())
-	flemming:SetNoDraw(true)
-	timer.Simple(10, function()
-		if isentity(flemming) and IsValid(flemming) then -- you won fair and square, guess i'm gone!
-			flemming:SetNoDraw(false)
-			flemming:Activate()
-			flemming:Spawn()
-		end
-	end)
+    local flemming = ents.Create(self:GetClass())
+    if !IsValid(self) then return end -- doesn't exist!
+    if !IsValid(flemming) then return end
+    flemming:SetPos(self:GetPos())
+    flemming:SetAngles(self:GetAngles())
+    flemming:SetMaterial(self:GetMaterial())
+    flemming:SetColor(self:GetColor())
+    flemming:SetName(self:GetName())
+    flemming:SetModel(self:GetModel())
+    flemming:SetNoDraw(true)
+    timer.Simple(10, function()
+        if isentity(flemming) and IsValid(flemming) then -- you won fair and square, guess i'm gone!
+            flemming:SetNoDraw(false)
+            flemming:Activate()
+            flemming:Spawn()
+        end
+    end)
 end
